e0ceb62dca99f99db77bfad1b9e38ca170dbef44
rain_local <jefri.yeh@outlook.com>
2017-10-28 21:52:17 +1300

    test deploy

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello BRAINYART!"
+    return "Hi Hi BRAINYART!"
 
 if __name__ == "__main__":
     application.run()
